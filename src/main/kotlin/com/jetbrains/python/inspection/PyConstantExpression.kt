57623a0a1b79d05e62ec13bbff382c73504d5edd
Roman Golyshev <anstreth@gmail.com>
2018-05-06 22:04:27 +0300

    add recognition of binary expressions with numeric literals

--- src/main/kotlin/com/jetbrains/python/inspection/PyConstantExpression.kt
+++ src/main/kotlin/com/jetbrains/python/inspection/PyConstantExpression.kt
@@ -5,20 +5,20 @@ import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
 import com.jetbrains.python.inspections.PyInspection
 import com.jetbrains.python.inspections.PyInspectionVisitor
-import com.jetbrains.python.psi.PyBoolLiteralExpression
-import com.jetbrains.python.psi.PyIfPart
-import com.jetbrains.python.psi.PyIfStatement
+import com.jetbrains.python.psi.*
 
+private typealias PyNum = PyNumericLiteralExpression
+private typealias PyBinOp = PyBinaryExpression
 
 class PyConstantExpression : PyInspection() {
 
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean,
+    override fun buildVisitor(holder: ProblemsHolder,
+                              isOnTheFly: Boolean,
                               session: LocalInspectionToolSession): PsiElementVisitor {
         return Visitor(holder, session)
     }
 
     class Visitor(holder: ProblemsHolder?, session: LocalInspectionToolSession) : PyInspectionVisitor(holder, session) {
-
         override fun visitPyIfStatement(node: PyIfStatement) {
             super.visitPyIfStatement(node)
             processIfPart(node.ifPart)
@@ -29,8 +29,43 @@ class PyConstantExpression : PyInspection() {
 
         private fun processIfPart(pyIfPart: PyIfPart) {
             val condition = pyIfPart.condition
-            if (condition is PyBoolLiteralExpression) {
-                registerProblem(condition, "The condition is always " + condition.value)
+            when (condition) {
+                is PyBoolLiteralExpression -> registerProblem(condition, "The condition is always " + condition.value)
+                is PyBinOp -> handleBinaryExpression(condition)
+            }
+        }
+
+        private fun handleBinaryExpression(binOp: PyBinOp) {
+            val evaledOp = evalBinaryExpressionToBool(binOp) ?: return
+
+            registerProblem(binOp, "The condition is always $evaledOp")
+        }
+
+        companion object {
+            fun evalBinaryExpressionToBool(condition: PyBinOp): Boolean? {
+                val left = condition.leftExpression ?: return null
+                val right = condition.rightExpression ?: return null
+                val op = condition.psiOperator?.text ?: return null
+
+                if (left is PyNum && right is PyNum) {
+                    val leftNum = left.bigDecimalValue ?: return null
+                    val rightNum = right.bigDecimalValue ?: return null
+
+                    return when (op) {
+                        ">" -> leftNum > rightNum
+                        ">=" -> leftNum >= rightNum
+
+                        "<" -> leftNum < rightNum
+                        "=<" -> leftNum <= rightNum
+
+                        "==" -> leftNum == rightNum
+                        "!=" -> leftNum != rightNum
+
+                        else -> null
+                    }
+                }
+
+                return null
             }
         }
     }
